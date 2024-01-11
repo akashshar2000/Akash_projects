8fc217f4681423f58ceae04d2d7955b2eb34140a
akashshar2000 <151987375+akashshar2000@users.noreply.github.com>
2024-01-11 17:29:06 +0530

    Add files via upload

---
+++ robospeaker.py
@@ -0,0 +1,10 @@
+# importing the pyttsx library
+import pyttsx3
+
+# initialisation
+engine = pyttsx3.init()
+
+# testing
+engine.say("Akash first code on Robospeaker")
+engine.say("bye bye, Akash")
+engine.runAndWait()
